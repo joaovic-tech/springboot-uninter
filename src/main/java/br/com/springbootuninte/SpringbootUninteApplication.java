c874b45f084cbc8022cb6815bcc6e3b969a51933
joaovic-tech <joaovictorca2004@gmail.com>
2024-05-06 19:39:21 -0300

    first commit

---
+++ src/main/java/br/com/springbootuninte/SpringbootUninteApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbootuninte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUninteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUninteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springbootuninte/SpringbootUninteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbootuninte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUninteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
